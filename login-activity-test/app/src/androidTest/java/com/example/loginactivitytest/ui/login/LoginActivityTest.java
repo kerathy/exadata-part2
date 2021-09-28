515efafaa98b29b11fb6e4cd2e68c1dff10fce97
kerathy <xxyxuxiaoyan@126.com>
2021-09-28 16:57:00 -0600

    test login button

---
+++ login-activity-test/app/src/androidTest/java/com/example/loginactivitytest/ui/login/LoginActivityTest.java
@@ -0,0 +1,36 @@
+package com.example.loginactivitytest.ui.login;
+
+import android.widget.Button;
+
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.loginactivitytest.R;
+
+import junit.framework.TestCase;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class LoginActivityTest {
+
+    @Rule
+    public IntentsTestRule<LoginActivity> loginActivityRule = new IntentsTestRule<>(LoginActivity.class);
+
+    @Test
+    public void test_loginButton_shouldHaveValidText() {
+        LoginActivity loginActivity = loginActivityRule.getActivity();
+        Assert.assertNotNull(loginActivity);
+
+        Button loginButton = loginActivity.findViewById(R.id.login);
+        Assert.assertNotNull(loginButton);
+
+        Assert.assertEquals(loginButton.getText().toString(), "Botton Text");
+
+    }
+
+
+}
