336ffc09f970787129653b7db8befb217822da8f
kerathy <xxyxuxiaoyan@126.com>
2021-09-28 23:00:37 -0600

    update

--- login-activity-test/app/src/androidTest/java/com/example/loginactivitytest/ui/login/LoginActivityTest.java
+++ login-activity-test/app/src/androidTest/java/com/example/loginactivitytest/ui/login/LoginActivityTest.java
@@ -4,7 +4,6 @@ import android.widget.Button;
 
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-
 import com.example.loginactivitytest.R;
 
 import junit.framework.TestCase;
@@ -16,6 +15,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class LoginActivityTest {
+
+    //https://stackoverflow.com/questions/43064681/what-is-the-difference-between-activitytestrule-and-intenttestrule
 
     @Rule
     public IntentsTestRule<LoginActivity> loginActivityRule = new IntentsTestRule<>(LoginActivity.class);
@@ -29,8 +30,5 @@ public class LoginActivityTest {
         Assert.assertNotNull(loginButton);
 
         Assert.assertEquals(loginButton.getText().toString(), "Botton Text");
-
     }
-
-
 }
